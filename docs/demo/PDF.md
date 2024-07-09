1856d20ed536ab9b5a0252ebae5f9e235eda714c
吴必炫 <2506823133@qq.com>
2024-07-09 21:40:40 +0800

    feat: 增加pdf例子

---
+++ docs/demo/PDF.md
@@ -0,0 +1,8 @@
+---
+title: PDF
+nav:
+  title: Demo
+  path: /demo
+---
+
+<code src="../example/PDF.tsx"></code>
---
+++ docs/example/PDF.tsx
@@ -0,0 +1,13 @@
+import React, {useEffect, useState} from 'react';
+import {fetchFile} from "../../src/z_utils";
+import { PDFViewer } from 'react-doc-view';
+
+const PDF = () => {
+  const [example, setExample] = useState<Blob>();
+  useEffect(() => {
+    fetchFile('/pdfFile.pdf', setExample);
+  }, []);
+  return <PDFViewer file={example} />;
+};
+
+export default PDF;
